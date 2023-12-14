1dadab0f7003b7c048cebe525acbb33b6d7703db
Tomas Snellman <towisnel@gmail.com>
2023-12-15 00:01:43 +0200

    Solution to 2023/14 part 2

--- 2023/day14/src/main.rs
+++ 2023/day14/src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
 use std::fs;
-use std::collections::BTreeMap;
+use std::collections::HashMap;
 use std::cmp::max;
+use itertools::Itertools;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -30,17 +31,153 @@ impl RockType {
     }
 }
 
-#[derive(Debug, Clone)]
-struct Rock {
-    x: i64,
-    y: i64,
-    t: RockType
+struct Game {
+    rocks: HashMap<(i64,i64), RockType>,
+    already_found: HashMap<String, i64>,
+    n_cols: i64,
+    n_rows: i64,
 }
 
-impl Rock {
-    fn mmove(&mut self, dx: &Vec<i64>, dy: &Vec<i64>) {
-        self.x += dx.get(self.x as usize).unwrap();
-        self.y += dy.get(self.y as usize).unwrap();
+// This is the cycle order
+#[derive(Debug, PartialEq, Eq)]
+enum Direction {
+    North,
+    West,
+    South,
+    East,
+}
+
+
+impl Game {
+    fn new(cont: &str) -> Game {
+        let line_width = cont.lines().next().expect("Should be at least 1 line").len() as i64 + 1;
+
+        let mut rocks: HashMap<(i64,i64), RockType> = HashMap::new();
+
+        let mut max_y = 0;
+        for (i,c) in cont.chars().enumerate() {
+            let y = (i as i64) / line_width;
+            max_y = max(y, max_y);
+            match c {
+                '.' | '\n' | ' ' => { continue; },
+                'O' | '#' => {
+                    let x = (i as i64) % line_width;
+                    let t = RockType::new(c);
+                    rocks.insert((x,y), t);
+                },
+                _ => { // Insert the Node but don't return
+                    panic!("Unknown character");
+                }
+            }
+        }
+        Game {rocks: rocks, already_found: HashMap::new(), n_cols: line_width - 1, n_rows: max_y + 1}
+    }
+
+    fn save(&mut self, i: i64) -> Option<i64> {
+        let tmp = self.print();
+        match self.already_found.get(&tmp) {
+            Some(j) => {
+                return Some(*j)
+            },
+            None => (),
+        }
+        self.already_found.insert(tmp, i);
+        None
+    }
+
+    fn print(&self) -> String {
+        (0..self.n_rows).map(|i_row| {
+            (0..self.n_cols).map(|i_col| {
+                match self.rocks.get(&(i_col, i_row)) {
+                    None => ".",
+                    Some(RockType::Round) => "O", 
+                    Some(RockType::Square) => "#", 
+                }
+            }).join("")
+        }).join("\n")
+    }
+
+    fn tilt(&mut self, direction: Direction) {
+        match direction {
+            Direction::North | Direction::South => {
+                for i_col in 0..(self.n_cols) {
+                    let mut prev_i: i64 = if direction == Direction::North {
+                        -1
+                    } else {
+                        self.n_rows 
+                    };
+                    let it: Box<dyn Iterator<Item = i64>> = if direction == Direction::North {
+                        Box::new(0..(self.n_rows))
+                    } else {
+                        Box::new((0..(self.n_rows)).rev())
+                    };
+                    let add = if direction == Direction::North {
+                        1
+                    } else {
+                        -1
+                    };
+                    for i_row in it {
+                        match self.rocks.get(&(i_col, i_row)) {
+                            None => continue,
+                            Some(RockType::Square) => {
+                                prev_i = i_row;
+                            },
+                            Some(RockType::Round) => {
+                                prev_i += add;
+                                if prev_i != i_row {
+                                    self.rocks.insert((i_col, prev_i), RockType::Round);
+                                    self.rocks.remove(&(i_col, i_row));
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            Direction::East | Direction::West => {
+                for i_row in 0..(self.n_rows) {
+                    let mut prev_i: i64 = if direction == Direction::West {
+                        -1
+                    } else {
+                        self.n_cols 
+                    };
+                    let it: Box<dyn Iterator<Item = i64>> = if direction == Direction::West {
+                        Box::new(0..(self.n_cols))
+                    } else {
+                        Box::new((0..(self.n_cols)).rev())
+                    };
+                    let add = if direction == Direction::West {
+                        1
+                    } else {
+                        -1
+                    };
+                    for i_col in it {
+                        match self.rocks.get(&(i_col, i_row)) {
+                            None => continue,
+                            Some(RockType::Square) => {
+                                prev_i = i_col;
+                            },
+                            Some(RockType::Round) => {
+                                prev_i += add;
+                                if prev_i != i_col {
+                                    self.rocks.insert((prev_i, i_row), RockType::Round);
+                                    self.rocks.remove(&(i_col, i_row));
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+
+        }
+    }
+
+    fn score(&self) -> i64{
+        self.rocks.iter().map(|((_,y), v)| {
+            match v {
+                RockType::Round => self.n_rows - y,
+                _ => 0
+            }
+        }).sum()
     }
 }
 
@@ -49,74 +186,50 @@ fn main() {
 
     let contents = fs::read_to_string(&args.input)
         .expect("Should have been able to read the file");
-    // In part 1 we add 1 one row/column for each empty one.
-    // In other words multiply amount of empty space by 2
-    let res1 = read_contents(&contents);
+
+    // 0 cycles means just one tilt to north (part1)
+    let res1 = read_contents(&contents, 0);
     println!("Part 1 answer is {}", res1);
-    // In part 2 we multiply the amount of empty space by 1000000
-    //let res2 = read_contents(&contents, 1e6 as i64);
-    //println!("Part 2 answer is {}", res2);
+
+    // In part2 we do 1e9 cycles
+    let res2 = read_contents(&contents, 1_000_000_000);
+    println!("Part 2 answer is {}", res2);
 }
 
 fn read_contents(cont: &str, cycles: i64) -> i64 {
-    // Expansion gives the multipliciation of empty space
-    // Adding 1 row, means multiplying the amount of empty space by 2
-    // Relatedly when expansion is N, we need to add N -1 rows/columns
-    let line_width = cont.lines().next().expect("Should be at least 1 line").len() as i64 + 1;
-
-    let mut cols: BTreeMap<i64, Vec<Rock>> = BTreeMap::new();
-    //let mut rows: Vec<> = HashSet::new();
-
-    let mut max_y = 0;
-    for (i,c) in cont.chars().enumerate() {
-        let mut y = (i as i64) / line_width;
-        max_y = max(y, max_y);
-        match c {
-            '.' | '\n' | ' ' => { continue; },
-            'O' | '#' => {
-                let x = (i as i64) % line_width;
-                let col = match cols.get_mut(&x) {
-                    None => {
-                        let c = Vec::new();
-                        cols.insert(x, c);
-                        cols.get_mut(&x).unwrap()
-                    },
-                    Some(v) => {
-                        v
-                    }
-                };
-                let t = RockType::new(c);
-                match (col.last(), &t) {
-                    (_, RockType::Square) => {
-                        y = (i as i64) / line_width;
-                    },
-                    (None, RockType::Round) => {
-                        y = 0;
-                    },
-                    (Some(v), RockType::Round) => {
-                        y = v.y +1;
+    let mut game = Game::new(cont);
+    if cycles == 0 {
+        game.tilt(Direction::North);
+    } else {
+        game.save(0);
+        for i in 0..cycles {
+            game.tilt(Direction::North);    
+            game.tilt(Direction::West);    
+            game.tilt(Direction::South);    
+            game.tilt(Direction::East);    
+            match game.save(i) {
+                None => (),
+                Some(j) => {
+                    println!("Already found at index {}, now at {}", j, i);
+                    println!("{}",game.print());
+                    if i > 2 {
+                        let cycle_length = i - j;
+                        let need_to_go = cycles - i;
+                        let left_to_go = need_to_go % cycle_length - 1;
+                        println!("{} steps left", left_to_go);
+                        for _ in 0..left_to_go {
+                            game.tilt(Direction::North);
+                            game.tilt(Direction::West);   
+                            game.tilt(Direction::South);
+                            game.tilt(Direction::East);
+                        }
+                        return game.score();
                     }
                 }
-                //let y = (i as i64) / line_width;
-                let r = Rock {x: x, y: y, t: t};
-                col.push(r);
-            },
-            _ => { // Insert the Node but don't return
-                panic!("Unknown character");
             }
         }
     }
-
-    dbg!(&cols);
-    cols.values().map(|col| {
-        col.iter().map(|r| {
-            match r.t {
-                RockType::Round => 1 + max_y - r.y,
-                _ => 0
-            }
-        }).sum::<i64>()
-    }
-    ).sum()
+    game.score()
 }
 
 #[cfg(test)]
@@ -138,6 +251,81 @@ O.#..O.#.#
         assert_eq!(read_contents(&a, 0), 136);
     }
 
+    #[test]
+    fn tilts() {
+        let mut game = Game::new("O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....");
+
+        assert_eq!(game.n_cols, 10);
+        assert_eq!(game.n_rows, 10);
+        //println!("{}\n", game.print());
+        game.tilt(Direction::North);
+        //println!("{}\n", game.print());
+        assert_eq!(game.print(), "OOOO.#.O..
+OO..#....#
+OO..O##..O
+O..#.OO...
+........#.
+..#....#.#
+..O..#.O.O
+..O.......
+#....###..
+#....#...."
+);
+
+        game.tilt(Direction::South);
+        assert_eq!(game.print(), ".....#....
+....#....#
+...O.##...
+...#......
+O.O....O#O
+O.#..O.#.#
+O....#....
+OO....OO..
+#OO..###..
+#OO.O#...O"
+);
+        println!("{}\n", game.print());
+        game.tilt(Direction::East);
+        println!("{}\n", game.print());
+
+        assert_eq!(game.print(), ".....#....
+....#....#
+....O##...
+...#......
+.....OOO#O
+.O#...O#.#
+....O#....
+......OOOO
+#..OO###..
+#.OOO#...O"
+);
+        println!("{}\n", game.print());
+        game.tilt(Direction::West);
+        println!("{}\n", game.print());
+
+        assert_eq!(game.print(), ".....#....
+....#....#
+O....##...
+...#......
+OOO.....#O
+O.#O...#.#
+O....#....
+OOOO......
+#OO..###..
+#OOO.#O..."
+);
+
+    }
+    #[test]
     fn part2() {
         let a = "O....#....
 O.OO#....#
