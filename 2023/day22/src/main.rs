f1a998c2c36c4aa1695b41aff4a66fe2f2d15050
Tomas Snellman <towisnel@gmail.com>
2023-12-22 15:15:57 +0200

    Solution to 2023/22

--- 2023/day22/src/main.rs
+++ 2023/day22/src/main.rs
@@ -1,14 +1,12 @@
 use clap::Parser;
 use std::fs;
-use std::collections::BTreeMap;
 use std::collections::HashSet;
 use std::collections::HashMap;
+use std::collections::VecDeque;
 use regex::Regex;
-use std::cmp::max;
 use std::fmt::Display;
 use core::fmt;
 use std::cmp::Ordering;
-use std::iter::zip;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -24,14 +22,14 @@ fn main() {
     let contents = fs::read_to_string(&args.input)
         .expect("Should have been able to read the file");
 
-    // 0 cycles means just one tilt to north (part1)
-    let res = part1(&contents);
-    println!("Part 1 answer is {}", res);
+    let res = read_contents(&contents);
+    println!("Part 1 answer is {}", res.0);
+    println!("Part 2 answer is {}", res.1);
 }
 
 
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Vec3D {
     x: i64,
     y: i64,
@@ -62,11 +60,13 @@ impl PartialOrd for Vec3D {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Block {
     start: Vec3D,
     end: Vec3D,
     name: String,
+    held_by: HashSet<String>,
+    holds: HashSet<String>,
 }
 
 
@@ -82,10 +82,19 @@ impl Display for Vec3D {
     }
 }
 
+static ASCII_UPPER: [char; 26] = [
+    'A', 'B', 'C', 'D', 'E', 
+    'F', 'G', 'H', 'I', 'J', 
+    'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 
+    'U', 'V', 'W', 'X', 'Y', 
+    'Z',
+];
+
 //0,2,3~2,2,3
 impl Block {
     fn new(input: &str) -> Block {
-        let re = Regex::new("([0-9]),([0-9]),([0-9])~([0-9]),([0-9]),([0-9])").unwrap();
+        let re = Regex::new("([0-9]*),([0-9]*),([0-9]*)~([0-9]*),([0-9]*),([0-9]*)").unwrap();
         let Some(res) = re.captures(input) else {panic!("Could not parse {}", input)};
         let start = Vec3D {
             x: res[1].parse::<i64>().unwrap(),
@@ -97,37 +106,42 @@ impl Block {
             y: res[5].parse::<i64>().unwrap(),
             z: res[6].parse::<i64>().unwrap(),
         };
-        dbg!(&input);
         let block = if start > end {
-            Block {start: end, end:start, name: "block".to_string()}
+            Block {
+                start: end,
+                end: start,
+                name: "block".to_string(),
+                held_by: HashSet::new(),
+                holds: HashSet::new(),
+            }
         } else {
-            Block {start: start, end:end, name: "block".to_string()}
+            Block {
+                start: start,
+                end: end,
+                name: "block".to_string(),
+                held_by: HashSet::new(),
+                holds: HashSet::new(),
+            }
         };
         block
     }
 
     fn overlaps(&self, other: &Block) -> bool {
-        println!("Self: {}", self);
-        println!("Other: {}", other);
         if (self.start.x <= other.end.x) & (self.end.x >= other.start.x) {
             if (self.start.y <= other.end.y) & (self.end.y >= other.start.y) {
                 return true
             }
         }
         if other.end.x < self.start.x {
-            println!("first");
             return false
         }
         if other.end.y < self.start.y {
-            println!("second");
             return false
         }
         if other.start.x > self.end.x {
-            println!("third");
             return false
         }
         if other.start.y > self.end.y {
-            println!("fourth");
             return false
         }
         return true
@@ -137,43 +151,113 @@ impl Block {
         self.start.z > other.end.z
     }
 
-    fn below(&self, other: &Block) -> bool {
-        !self.above(other)
+    fn drop(&mut self, steps: i64) {
+        self.start.z -= steps;
+        self.end.z -= steps;
     }
 }
 
-fn part1(cont: &str) -> i64 {
+fn read_contents(cont: &str) -> (i64, i64) {
     let mut blocks: Vec<Block> = cont.lines().map(|ln| {
         Block::new(ln)
     }).collect();
 
-    let names = ["A", "B", "C", "D", "E", "F", "G"];
-    for i in (0..names.len()) {
-        blocks[i].name = names[i].to_string();
+    // Give names to blocks
+    // Example used A-F. Thus gives names AAA-ZZZ
+    for i in 0..blocks.len() {
+        let (div, a) = (i / 26, i % 26);
+        let (div, b) = (div / 26, div % 26);
+        let (_div, c) = (div / 26, div % 26);
+        let mtp = format!("{}{}{}", ASCII_UPPER[c], ASCII_UPPER[b], ASCII_UPPER[a]);
+        blocks[i].name = mtp;
+    }
+    let n = blocks.len();
+
+    // When moving the blocks downwards, we assume that the list is in increasin z order
+    blocks.sort_by_key(|b| b.start.z);
+
+    // Below name, and above name
+    let mut hold_relations: HashSet<(String, String)> = HashSet::new();
+
+    // Move blocks downwards and take not of which blocks hold which ones
+    for i in 0..n {
+        let mut b = blocks[i].clone();
+        let hold_list = (0..n).filter_map(|j| {
+            if i == j {
+                return None
+            }
+            let b_comp = &blocks[j];
+            let is_above = b.overlaps(&b_comp) & b.above(&b_comp);
+            if is_above {
+                //println!("Block {} is above block {}", &b, &b_comp);
+                Some((b_comp.name.clone(), b_comp.end.z.clone()))
+            } else {
+                None
+            }
+        }).collect::<Vec<_>>();
+        let max_z = hold_list.iter().map(|(_, z)| z.clone()).max().unwrap_or(0);
+        for (name, z) in &hold_list {
+            if z == &max_z {
+                hold_relations.insert((name.to_string(), b.name.to_string()));
+            }
+        }
+        if max_z == 0 {
+            hold_relations.insert(("GROUND".to_string(), b.name.to_string()));
+        }
+        let new_z = max_z + 1;
+        b.drop(b.start.z - new_z);
+        blocks[i] = b;
     }
 
-    let mut removable: HashSet<Block> = HashSet::new();
-    blocks.iter().for_each(|b| {
-        // Want list of blocks that are above b
-        let tmp = blocks.iter().filter(|b_comp| {
-            if &b == b_comp {
-                return false
-            }
-            let is_below = b.overlaps(&b_comp) & b.below(&b_comp);
-            if is_below {
-                println!("Block {} is below block {}", b, b_comp);
-            }
-            is_below
-        }).collect::<Vec<_>>();
-        if tmp.len() > 1 {
-            for t in tmp {
-                removable.insert(t.clone());
-            }
-        }});
-    for t in &removable {
-        println!("Block {} can be removed", &t);
+    // Convert vector to hashmap for easier retrieval
+    let mut block_map: HashMap<String, Block> = blocks.into_iter().map(|b| (b.name.clone(), b)).collect();
+    for (name_below, name_above) in hold_relations {
+        if name_below != "GROUND" {
+            let below = block_map.get_mut(&name_below).unwrap();
+            below.holds.insert(name_above.to_string());
+        }
+        let above = block_map.get_mut(&name_above).unwrap();
+        above.held_by.insert(name_below.to_string());
     }
-    removable.len() as i64
+
+    let mut part1 = 0;
+    for (_name, block) in &block_map {
+        if block.holds.iter().filter(|b_name|{
+            let b = block_map.get(&b_name.to_string()).unwrap();
+            b.held_by.len() == 1
+        }).count() == 0 {
+            part1 += 1;
+        }
+    }
+
+    let mut drops = 0;
+    let mut i = 0;
+    let n = block_map.len();
+    for name in block_map.keys() {
+        println!("{:04} / {:04}, Dropping {}", i+1,n,&name);
+        i += 1;
+        let mut bmap = block_map.clone();
+        let mut dropped_blocks: VecDeque<String> = VecDeque::new();
+        dropped_blocks.push_back(name.to_string());
+        loop {
+            let dropped_name = match dropped_blocks.pop_front() {
+                Some(v) => v,
+                None => break,
+            };
+            let dropped_block = block_map.get(&dropped_name).unwrap();
+            for above_name in &dropped_block.holds {
+                let above_block = bmap.get_mut(above_name).unwrap();
+                if above_block.held_by.contains(&dropped_name) {
+                    above_block.held_by.remove(&dropped_name);
+                    if above_block.held_by.len() == 0{
+                        dropped_blocks.push_back(above_name.clone());
+                        drops += 1;
+                    }
+                }
+            }
+        }
+    }
+    (part1, drops)
 }
 
 #[cfg(test)]
@@ -196,42 +280,39 @@ mod tests {
         assert!(a.overlaps(&Block::new("1,0,1~1,0,1")));
         assert!(a.overlaps(&Block::new("0,0,1~0,2,1")));
 
-        let A = Block::new("1,0,1~1,2,1");
-        let B = Block::new("0,0,2~2,0,2");
-        let C = Block::new("0,2,3~2,2,3");
-        let D = Block::new("0,0,4~0,2,4");
-        let E = Block::new("0,1,6~2,1,6");
-        let F = Block::new("2,0,5~2,2,5");
-        let G = Block::new("1,1,8~1,1,9");
-        assert!(A.overlaps(&B));
-        assert!(A.overlaps(&C));
+        let a = Block::new("1,0,1~1,2,1");
+        let b = Block::new("0,0,2~2,0,2");
+        let c = Block::new("0,2,3~2,2,3");
+        let d = Block::new("0,0,4~0,2,4");
+        let e = Block::new("0,1,6~2,1,6");
+        let f = Block::new("2,0,5~2,2,5");
+        let g = Block::new("1,1,8~1,1,9");
+        assert!(a.overlaps(&b));
+        assert!(a.overlaps(&c));
 
-        assert!(B.overlaps(&D));
-        println!("{}", B);
-        println!("{}", E);
-        assert!(E.overlaps(&B));
-        assert!(B.overlaps(&E));
+        assert!(b.overlaps(&d));
+        assert!(!e.overlaps(&b));
+        assert!(!b.overlaps(&e));
 
-        assert!(C.overlaps(&D));
-        assert!(C.overlaps(&E));
+        assert!(c.overlaps(&d));
+        assert!(!c.overlaps(&e));
 
-        assert!(D.overlaps(&F));
-        assert!(E.overlaps(&F));
+        assert!(!d.overlaps(&f));
+        assert!(e.overlaps(&f));
 
-        assert!(F.overlaps(&G));
+        assert!(!f.overlaps(&g));
     }
 
     #[test]
     fn conts() {
-        let a = "
-1,0,1~1,2,1
+        let a = "1,0,1~1,2,1
 0,0,2~2,0,2
 0,2,3~2,2,3
 0,0,4~0,2,4
 2,0,5~2,2,5
 0,1,6~2,1,6
 1,1,8~1,1,9";
-        assert_eq!(part1(&a), 5);
+        assert_eq!(read_contents(&a).0, 5);
+        assert_eq!(read_contents(&a).1, 7);
     }
-
 }
