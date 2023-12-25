3d7fe50fbde8d200cd5296c10e44fb669e77efd3
Tomas Snellman <towisnel@gmail.com>
2023-12-26 00:44:10 +0200

    Solution to 2023/25

--- 2023/day25/src/main.rs
+++ 2023/day25/src/main.rs
@@ -3,9 +3,11 @@ use clap::Parser;
 use std::fs;
 use std::collections::BTreeMap;
 use std::collections::BTreeSet;
-use itertools::Itertools;
+use ndarray::{Array2, Axis};
+use ndarray_linalg::Eig;
+#[allow(clippy::cast_precision_loss, clippy::cast_possible_truncation)]
 
-#[allow(clippy::cast_possible_truncation)]
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -14,7 +16,7 @@ struct Args {
     input: String
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct Node {
     name: String,
     connections: BTreeSet<String>,
@@ -46,17 +48,13 @@ fn main() {
 
     let res = read_contents(&contents);
     println!("Part 1 answer is {res}");
-
-    //let res = read_contents(&contents);
-    //println!("Part 2 answer is {res}");
-
 }
 
 fn parse_line(line: &str) -> Vec<(String, String)> {
-    let mut a = line.split(":");
+    let mut a = line.split(':');
     let from = a.next().unwrap();
-    let tmp = a.next().unwrap().split(" ").filter_map(|x| {
-        if x == "" {
+    let tmp = a.next().unwrap().split(' ').filter_map(|x| {
+        if x.is_empty() {
             None
         } else {
             Some((from.to_string(), x.to_string()))
@@ -66,77 +64,104 @@ fn parse_line(line: &str) -> Vec<(String, String)> {
 }
 
 
-fn get_size(start_node: String, nodes: &BTreeMap<String, Node>) -> i64 {
-    let start = &nodes.get(&start_node).unwrap();
+fn get_size(start_node: &str, nodes: &BTreeMap<String, Node>) -> i64 {
+    let start = &nodes.get(start_node).unwrap();
     let mut visited: BTreeSet<&String> = BTreeSet::new();
     let mut nodes_to_visit: Vec<&String> = vec![&start.name];
-    loop {
-        let node = match nodes_to_visit.pop() {
-            Some(v) => nodes.get(v).unwrap(),
-            None => break,
-        };
+    while let Some(v) = nodes_to_visit.pop() {
+        let node = nodes.get(v).unwrap();
         visited.insert(&node.name);
         for t in &node.connections {
             if !visited.contains(&t) {
-                nodes_to_visit.push(&t);
+                nodes_to_visit.push(t);
             }
         }
     }
-    visited.len() as i64
+    i64::try_from(visited.len()).unwrap()
 }
 
 fn is_connected(nodes: &BTreeMap<String, Node>) -> bool {
     let start = &nodes.values().next().unwrap();
     let mut visited: BTreeSet<&String> = BTreeSet::new();
     let mut nodes_to_visit: Vec<&String> = vec![&start.name];
-    loop {
-        let node = match nodes_to_visit.pop() {
-            Some(v) => nodes.get(v).unwrap(),
-            None => break,
-        };
+    while let Some(v) = nodes_to_visit.pop() {
+        let node = nodes.get(v).unwrap();
         visited.insert(&node.name);
         for t in &node.connections {
             if !visited.contains(&t) {
-                nodes_to_visit.push(&t);
+                nodes_to_visit.push(t);
             }
         }
     }
     if nodes.keys().all(|k| visited.contains(&k)) {
         //println!("Graph is connected");
-        return true;
+        true
     } else {
         //println!("Graph is not connected");
-        return false;
+        false
     }
 }
 
 fn read_contents(cont: &str) -> i64 {
-    let mut connections = cont.lines().map(|c| {
+    let connections = cont.lines().flat_map(|c| {
         parse_line(c)
-    }).flatten().collect::<Vec<_>>();
+    }).collect::<Vec<_>>();
     println!("{} Connections", connections.len());
     let mut nodes: BTreeMap<String, Node> = BTreeMap::new();
-    let n = connections.len();
     for (start, end) in &connections {
-        let start_node = match nodes.get_mut(start) {
-            Some(v) => v,
-            None => {
-                let node = Node::new(start.clone());
-                nodes.insert(start.clone(), node);
-                nodes.get_mut(start).unwrap()
-            }
+        let start_node = if let Some(v) = nodes.get_mut(start) { v } else {
+            let node = Node::new(start.clone());
+            nodes.insert(start.clone(), node);
+            nodes.get_mut(start).unwrap()
         };
         start_node.push(end.clone());
-        let end_node = match nodes.get_mut(end) {
-            Some(v) => v,
-            None => {
-                let node = Node::new(end.clone());
-                nodes.insert(end.clone(), node);
-                nodes.get_mut(end).unwrap()
-            }
+        let end_node = if let Some(v) = nodes.get_mut(end) { v } else {
+            let node = Node::new(end.clone());
+            nodes.insert(end.clone(), node);
+            nodes.get_mut(end).unwrap()
         };
         end_node.push(start.clone());
     }
+    println!("{} Nodes", nodes.len());
+    laplacian(&nodes)
+}
+
+
+// 567606 is the answer
+fn laplacian(nodes: &BTreeMap<String, Node>) -> i64{
+    let n = nodes.len();
+    let mut laplacian = Array2::<f64>::zeros((n, n));
+    let keys = nodes.iter().enumerate().map(|(i, (name, _node))| {
+    (i, name)
+    }).collect::<BTreeMap<_,_>>();
+    let inv_keys = keys.iter().map(|(i,name)| (*name, *i)).collect::<BTreeMap<_,_>>();
+    for i in 0..n {
+        let name = keys.get(&i).unwrap();
+        let node = nodes.get(&name.to_string()).unwrap();
+        laplacian[[i,i]] = node.connections.len() as f64;
+        for t in &node.connections {
+            let j = inv_keys.get(&t).unwrap();
+            laplacian[[i,*j]] = -1.0;
+            laplacian[[*j,i]] = -1.0;
+        }
+    }
+    let (eigs, vecs) = laplacian.eig().unwrap();
+    let vectors = vecs.axis_iter(Axis(1)).collect::<Vec<_>>();
+    let mut eigs_vectors = vectors.iter().zip(eigs.iter()).map(|(v, e)| (e.re, v)).collect::<Vec<_>>();
+    eigs_vectors.sort_by(|(e,_v), (e1,_v1)| e.partial_cmp(e1).unwrap());
+    let (_e2, second) = eigs_vectors.get(1).unwrap();
+    let tmp = second.iter().enumerate().map(|(i,v)| (keys.get(&i).unwrap(), v.re)).collect::<BTreeMap<_,_>>();
+    assert_eq!(tmp.len(), n);
+    let positive = i64::try_from(
+        tmp.values().filter(|v| **v > 0.0).count()
+    ).unwrap();
+    positive * (i64::try_from(n).unwrap() - positive)
+    
+}
+
+fn brute_force(mut nodes: BTreeMap<String, Node>, connections: &Vec<(String, String)>) -> i64{
+    // Brute force solution. Works with the test data, but way too slow for actual solution
+    let n = connections.len();
     for i in 0..n {
         if i % 10 == 0 {
             println!("{} / {n}", i + 1);
@@ -181,8 +206,8 @@ fn read_contents(cont: &str) -> i64 {
                     dbg!(&connections[j]);
                     dbg!(&connections[k]);
                     let (start, end) = &connections[i];
-                    let a = get_size(start.to_string(), &nodes);
-                    let b = get_size(end.to_string(), &nodes);
+                    let a = get_size(start, &nodes);
+                    let b = get_size(end, &nodes);
                     return a * b;
                 };
                 {   let node_start_k = nodes.get_mut(start_k).unwrap();
@@ -209,8 +234,6 @@ fn read_contents(cont: &str) -> i64 {
             node_end_i.push(start_i.to_string());
         }
     }
-    //dbg!(&nodes.len());
-    //dbg!(&nodes);
     0
 }
 
