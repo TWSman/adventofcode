fab0b6338f266f91e4f24c1b31b0ac6a4a341f72
Tomas Snellman <towisnel@gmail.com>
2024-12-04 12:55:48 +0200

    Day4 part2

--- 2024/day4/src/main.rs
+++ 2024/day4/src/main.rs
@@ -22,12 +22,22 @@ enum Letter {
 
 impl Letter {
     fn expect_next(&self) -> Option<Letter> {
-        match &self {
+        match self {
             Letter::X => Some(Letter::M),
             Letter::M => Some(Letter::A),
             Letter::A => Some(Letter::S),
             Letter::S => None,
             Letter::Other => None,
+        }
+    }
+
+    fn to_char(&self) -> char {
+        match self {
+            Letter::X => 'X',
+            Letter::M => 'M',
+            Letter::A => 'A',
+            Letter::S => 'S',
+            Letter::Other => '_',
         }
     }
 }
@@ -59,13 +69,13 @@ impl Directions {
     }
 }
 
-fn parseLetter(ltr: char) -> Letter {
+fn parse_letter(ltr: char) -> Letter {
     match ltr {
         'X' => Letter::X,
         'M' => Letter::M,
         'A' => Letter::A,
         'S' => Letter::S,
-        other => {dbg!(&other); Letter::Other},
+        _ => {Letter::Other},
     }
 }
 
@@ -78,27 +88,24 @@ fn main() {
     println!("Part 2 answer is {}", part2); 
 }
 
-fn get_element<T>(grid: &Vec<Vec<T>>, i:usize, j: usize) -> Option<&T> {
+fn get_element<T>(grid: &[Vec<T>], i:usize, j: usize) -> Option<&T> {
     grid.get(i)?.get(j)
 }
 
 fn get_part1(cont: &str) -> (i64, i64) {
     let grid = cont.lines().map(|ln| {
-        ln.chars().map(parseLetter).collect::<Vec<Letter>>()
+        ln.chars().map(parse_letter).collect::<Vec<Letter>>()
     }).collect::<Vec<Vec<Letter>>>();
-    let x_vals = grid.iter().enumerate().map(|(i, row)| {
+    let x_vals = grid.iter().enumerate().flat_map(|(i, row)| {
         row.iter().enumerate().filter_map(|(j, ltr)| {
             match ltr {
                 Letter::X => Some((i as i64,j as i64)),
                 _ => None,
             }
         }).collect::<Vec<(i64,i64)>>()
-    }).flatten().collect::<Vec<(i64, i64)>>();
-    dbg!(&x_vals);
+    }).collect::<Vec<(i64, i64)>>();
     let part1 = x_vals.iter().map(|(i,j)| {
-        dbg!(&(i,j));
         Directions::iter().map(|dir| {
-            dbg!(&dir);
             let add = dir.get_dir();
             let mut coord = (*i,*j);
             let mut expect_next = Letter::X.expect_next();
@@ -121,21 +128,34 @@ fn get_part1(cont: &str) -> (i64, i64) {
             1
         }).sum::<i64>()
     }).sum();
-    let a_vals = grid.iter().enumerate().map(|(i, row)| {
+    let a_vals = grid.iter().enumerate().flat_map(|(i, row)| {
         row.iter().enumerate().filter_map(|(j, ltr)| {
             match ltr {
                 Letter::A => Some((i as i64,j as i64)),
                 _ => None,
             }
         }).collect::<Vec<(i64,i64)>>()
-    }).flatten().collect::<Vec<(i64, i64)>>();
+    }).collect::<Vec<(i64, i64)>>();
     let part2 = a_vals.iter().map(|(i,j)| {
-        let check_these = vec![(-1,-1), (-1,1), (1,1), (1, -1)];
-        //let cross_vals = 
+        // NE, NW, SE, SW
+        let check_these = [(-1,-1), (-1,1), (1,1), (1, -1)];
+        let str = check_these.iter().filter_map(|(i_add, j_add)| {
+            let i_new = i + i_add;
+            let j_new = j + j_add;
+            Some(get_element(&grid, i_new as usize, j_new as usize)?.to_char())
+
+        }).collect::<String>();
+        //  let str = letters.iter().map(|ltr| ltr.to_char()).collect::<String>();
+        match str.as_str() {
+            "MMSS" => 1, // M's at top, S at bottom
+            "MSSM" => 1, // M's at left, S at right
+            "SMMS" => 1, // M's at right, S at left
+            "SSMM" => 1, // M's at bottom, S at top
+            _ => 0,
+        }
     }).sum();
     (part1, part2)
 }
-
 
 #[cfg(test)]
 mod tests {
