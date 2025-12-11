4a28825d51be1d1b82c178aa30404c07f93cda40
Tomas Snellman <towisnel@gmail.com>
2025-12-11 16:52:20 +0200

    2025 day 11, part2 works

--- 2025/day11/src/main.rs
+++ 2025/day11/src/main.rs
@@ -14,23 +14,20 @@ struct Args {
 
 #[derive(Debug, Clone)]
 struct Node {
-    name: String,
     targets: BTreeSet<String>,
     sources: BTreeSet<String>,
 }
 
 impl Node {
-    fn new(name: String) -> Self{
-        Self {name, targets: BTreeSet::new(), sources: BTreeSet::new()}
+    fn new() -> Self{
+        Self {targets: BTreeSet::new(), sources: BTreeSet::new()}
     }
-
 
     fn parse_line(ln: &str) -> (String, Self) {
         let (name, conns) = ln.split_once(':').unwrap();
         (
             name.to_string(),
             Self {
-                name: name.to_string(),
                 sources: BTreeSet::new(),
                 targets: conns.split(' ').filter_map(|x| {
                     if x.is_empty() {
@@ -53,9 +50,6 @@ fn main() {
     let res = read_contents(&contents);
     println!("Part 1 answer is {}", res.0);
     println!("Part 2 answer is {}", res.1);
-
-    // 208404 is too low
-    // 300 000 is too low
 }
 
 fn read_nodes(cont: &str) -> BTreeMap<String, Node> {
@@ -63,7 +57,7 @@ fn read_nodes(cont: &str) -> BTreeMap<String, Node> {
         Node::parse_line(ln)
     }).collect::<BTreeMap<String, Node>>();
 
-    nodes.insert("out".to_string(), Node::new("out".to_string()));
+    nodes.insert("out".to_string(), Node::new());
     
     let keys = nodes.keys().map(|v| v.to_owned()).collect::<Vec<_>>();
 
@@ -80,12 +74,8 @@ fn read_nodes(cont: &str) -> BTreeMap<String, Node> {
 fn read_contents(cont: &str) -> (i64, i64) {
     let nodes = read_nodes(cont);
 
-    dbg!(&nodes);
-
     println!("{} Nodes", nodes.len());
     let part1 = get_part1(&nodes);
-    dbg!(&part1);
-    //let part1 = 0;
     let part2 = get_part2(&nodes);
     dbg!(&part2);
     
@@ -93,14 +83,19 @@ fn read_contents(cont: &str) -> (i64, i64) {
 }
 
 fn get_part1(nodes: &BTreeMap<String, Node>) -> i64 {
-    get_route(nodes, "you", "out")
+    // Check results with two different methods
+    // First old but slow method. Still fast enough for part 1
+    let a = get_route(nodes, "you", "out");
+    // New optimized method
+    let b = get_route_solver(nodes, "you", "out");
+    assert_eq!(a,b);
+    a
 }
 
 fn get_part2(nodes: &BTreeMap<String, Node>) -> i64 {
-    let dac_to_fft = get_route(nodes, "dac", "fft");
-    dbg!(&dac_to_fft);
-    let fft_to_dac = get_route(nodes, "fft", "dac");
-    dbg!(&fft_to_dac);
+    // Check which node would come first
+    let dac_to_fft = get_route_solver(nodes, "dac", "fft");
+    let fft_to_dac = get_route_solver(nodes, "fft", "dac");
 
     let (inter1, inter2);
     let inter_count;
@@ -119,74 +114,66 @@ fn get_part2(nodes: &BTreeMap<String, Node>) -> i64 {
         inter2 = "dac";
         inter_count = fft_to_dac;
     } else {
-        panic!("No inter routes found");
+        panic!("No intermediate routes found");
     }
 
-    let start_to_inter = get_route(nodes, "svr", inter1);
-    let inter_to_end = get_route(nodes, inter2, "out");
+    // Get number of routes from start to first intermediate point
+    let start_to_inter = get_route_solver(nodes, "svr", inter1);
+    // And number of routes from second intermediate point to end
+    let inter_to_end = get_route_solver(nodes, inter2, "out");
     
+    // Total number of routes is product of three parts
     start_to_inter * inter_count * inter_to_end
 }
 
-fn get_route_inv(nodes: &BTreeMap<String, Node>, start: &str , end: &str) -> i64 {
-    dbg!(&end);
-    let mut heads: Vec<&str> = Vec::new();
-    heads.push(end);
-    let mut routes_to_target: BTreeMap<String, usize> = BTreeMap::new();
-    routes_to_target.insert(end.to_string(), 1);
-    loop {
-        if heads.len() == 0 {
-            break;
-        }
-        dbg!(&heads);
-        let head = heads.pop().unwrap();
-        // Get the number of routes from this node to end
-        let r_to_target = routes_to_target.get(head).unwrap().clone();
-        dbg!(&r_to_target);
-
-        for source in nodes.get(head).unwrap().sources.iter() {
-            let mut old_val: usize = 0;
-            if routes_to_target.contains_key(source) {
-                old_val = *routes_to_target.get(source).unwrap();
-            } else {
-
-            }
-            routes_to_target.insert(source.to_string(), old_val + r_to_target);
-            heads.push(source);
-        }
-    }
-    0
+struct Solver {
+    nodes: BTreeMap<String, Node>,
+    memory: BTreeMap<String, i64>, // Keep memory of how many routes from node to end
+    end: String,
 }
 
-//fn get_route_rec(nodes: &BTreeMap<String, Node>, start: &str , end: &str) -> i64 {
-//    let mut out = 0;
-//    for target in nodes.get(start).unwrap().targets.iter() {
-//        if target == end {
-//            out += 1;
-//        } else if target == "out" {
-//            //return get_route_rec(nodes, target, end);
-//        } else {
-//            out += get_route_rec(nodes, target, end);
-//        }
-//    }
-//}
+impl Solver {
+    fn new(nodes: BTreeMap<String, Node>, end: &str) -> Self {
+        Self {nodes, memory: BTreeMap::new(), end: end.to_string()}
+    }
+
+    fn solve(&mut self, start: &str) -> i64 {
+        let mut routes: i64 = 0;
+        let start_node = self.nodes.get(start).unwrap().clone();
+        for target in start_node.targets.iter() {
+            if target == &self.end {
+                routes += 1;
+            } else if target == "out" {
+                // This is a dead end. No point in continuing
+            } else {
+                let sub_routes: i64 = if self.memory.contains_key(target) {
+                    *self.memory.get(target).unwrap()
+                } else {
+                    self.solve(target)
+                };
+                routes += sub_routes;
+            }
+        }
+        // Store result in memory
+        self.memory.insert(start.to_string(), routes);
+        routes
+    }
+}
+
+
+fn get_route_solver(nodes: &BTreeMap<String, Node>, start: &str , end: &str) -> i64 {
+    let mut solver = Solver::new(nodes.clone(),end);
+    solver.solve(start)
+}
 
 fn get_route(nodes: &BTreeMap<String, Node>, start: &str , end: &str) -> i64 {
     let mut heads: Vec<(&str, usize, Vec<String>)> = Vec::new();
     heads.push((start, 0, vec![start.to_string()]));
     let mut routes = 0;
-    //let route_ln
-    let mut loop_count: usize = 0;
+    let mut visited_nodes: BTreeSet<String> = BTreeSet::new();
     loop {
-        //dbg!(&heads);
-        loop_count += 1;
-        if heads.len() == 0 {
+        if heads.is_empty() {
             break;
-        }
-        if loop_count % 1_000_000== 0 {
-            println!("Loop count {}", loop_count);
-            dbg!(&heads.len());
-            dbg!(routes);
         }
         let (head,n,history) = heads.pop().unwrap();
         if !nodes.contains_key(head) {
@@ -196,8 +183,10 @@ fn get_route(nodes: &BTreeMap<String, Node>, start: &str , end: &str) -> i64 {
         for target in node.targets.iter() {
             if target == end {
                 routes += 1;
-                //println!("Found route to out via");
-                //dbg!(&history);
+                for v in history.iter() {
+                    visited_nodes.insert(v.to_string());
+                }
+                //println!("Found route: {:?}", history);
             } else if target != "out" {
                 let mut new_history = history.clone();
                 new_history.push(target.to_string());
@@ -205,6 +194,8 @@ fn get_route(nodes: &BTreeMap<String, Node>, start: &str , end: &str) -> i64 {
             }
         }
     }
+    println!("Visited {} nodes between '{}' and '{}'l", visited_nodes.len(), start, end);
+    dbg!(&visited_nodes);
     routes
 }
 
@@ -226,6 +217,8 @@ ggg: out
 hhh: ccc fff iii
 iii: out";
         let nodes = read_nodes(&a);
+        assert_eq!(get_route(&nodes,"you", "out"), 5);
+        assert_eq!(get_route_solver(&nodes,"you", "out"), 5);
         assert_eq!(get_part1(&nodes), 5);
     }
 
