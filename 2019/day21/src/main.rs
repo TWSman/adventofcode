a0fecc6e596d61e155606217a83f3facb979aec2
Tomas Snellman <towisnel@gmail.com>
2026-03-10 16:29:04 +0200

    2019 day 21 part 2

--- 2019/day21/src/main.rs
+++ 2019/day21/src/main.rs
@@ -1,7 +1,5 @@
 use clap::Parser;
-use colored::Colorize;
 use intcode::*;
-use std::collections::BTreeMap;
 use std::fs;
 use std::time::Instant;
 
@@ -27,21 +25,16 @@ fn main() {
     println!("Execution lasted {:.2?}", elapsed);
 }
 
-
 fn get_part1(program: &Program) -> i64 {
     let mut p = program.clone();
-    let mut input_str = "NOT D T\n".to_string(); // Check that jump target is not empty, set to T
-    input_str += "NOT T J\n"; // Move ~T to J, if D was true, J should now be true
-    input_str += "NOT A T\n"; // If A is empty, set T to true
-    input_str += "AND T J\n"; // If A is empty, and D was true, J should still be true
-    // J should now be D && !A
-    
-    input_str += "NOT E T\n"; // If C is empty, set T to true
-    // T should now be !C
-    input_str += "AND D T\n";
-    // T should now be !C AND D
-    input_str += "OR T J\n";
+    let mut input_str = "OR A J\n".to_string(); // J should now be A
+    input_str += "AND B J\n"; // J should now be A AND B
+    input_str += "AND C J\n"; // J should now be A AND C
+    input_str += "NOT J J\n"; // J should now be ~(A AND C) = ~A OR ~C
+    input_str += "AND D J\n"; // J should now be D AND ~(A AND C)
     input_str += "WALK\n";
+
+    // J = D && (!(A AND B AND C)) // jump if target is not empty, and either A or B or C is empty
 
     println!("Input program:\n{}", input_str);
 
@@ -55,6 +48,13 @@ fn get_part1(program: &Program) -> i64 {
     // Chech final output
     let final_output = p.get_output(-1);
     if final_output > 127 {
+        let n = p.get_outputs();
+        let output = n[..n.len() - 1]
+            .iter()
+            .map(|&o| o as u8 as char)
+            .collect::<String>();
+        println!("Program prints:\n{}", output);
+
         println!("Final output: {}", final_output);
         return final_output;
     }
@@ -64,19 +64,28 @@ fn get_part1(program: &Program) -> i64 {
 }
 
 fn get_part2(program: &Program) -> i64 {
-    // WIP
     let mut p = program.clone();
-    let mut input_str = "NOT D T\n".to_string(); // Check that jump target is not empty, set to T
-    input_str += "NOT T J\n"; // Move ~T to J, if D was true, J should now be true
-    input_str += "NOT A T\n"; // If A is empty, set T to true
-    input_str += "AND T J\n"; // If A is empty, and D was true, J should still be true
-    // J should now be D && !A
-    
-    input_str += "NOT C T\n"; // If C is empty, set T to true
-    // T should now be !C
-    input_str += "AND D T\n";
-    // T should now be !C AND D
-    input_str += "OR T J\n";
+
+    // First segment checks that we can continue moving after the jump, either by a further jump, or
+    // by taking a step. Thus either the next tile must have ground, or the next jump target must
+    // have ground
+    // i.e. (E OR H)
+    let mut input_str = "OR E J\n".to_string(); // J = E
+    input_str += "OR H J\n"; // J = E OR H
+
+    // Next segment checks that jumping actually makes sense, i.e. there is an empty space between
+    // current space and target space
+    // i.e. ~A OR ~B OR ~C
+    input_str += "OR A T\n"; // T = A
+    input_str += "AND B T\n"; // T = A AND B
+    input_str += "AND C T\n"; // T = A AND B AND C
+    input_str += "NOT T T\n"; // T = ~(A AND B AND C) = ~A OR ~B OR ~C
+    input_str += "AND T J\n"; // 
+    // (~A OR ~B OR ~C) AND (E OR H)
+
+    // Finally check that target space is safe to jump to, i.e. D is true
+    input_str += "AND D J\n";
+    // (~A OR ~B OR ~C) AND (E OR H) AND D
     input_str += "RUN\n";
 
     println!("Input program:\n{}", input_str);
@@ -91,6 +100,14 @@ fn get_part2(program: &Program) -> i64 {
     // Chech final output
     let final_output = p.get_output(-1);
     if final_output > 127 {
+        // If the output is not valid ASCII,
+        let n = p.get_outputs();
+        let output = n[..n.len() - 1]
+            .iter()
+            .map(|&o| o as u8 as char)
+            .collect::<String>();
+        println!("Program prints:\n{}", output);
+
         println!("Final output: {}", final_output);
         return final_output;
     }
@@ -98,7 +115,6 @@ fn get_part2(program: &Program) -> i64 {
     println!("{}", output);
     0
 }
-
 
 fn read_contents(cont: &str) -> (i64, i64) {
     let vals = cont
@@ -111,4 +127,3 @@ fn read_contents(cont: &str) -> (i64, i64) {
     let part2 = get_part2(&p);
     (part1, part2)
 }
-
