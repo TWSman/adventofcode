102ff9cd35b4fee0c01500ff1ab1324c53aa28ca
Tomas Snellman <towisnel@gmail.com>
2026-04-04 22:14:24 +0300

    2015 day 12 part 2

--- 2015/day12/src/main.rs
+++ 2015/day12/src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
+use regex::Regex;
 use std::fs;
 use std::time::Instant;
-use regex::Regex;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -20,12 +20,12 @@ fn main() {
     println!("Part 1 answer is {}", res.0);
     println!("Part 2 answer is {}", res.1);
     let elapsed = start.elapsed();
-    println!("Execution lasted {:.2?}", elapsed);
+    println!("Execution lasted {elapsed:.2?}");
 }
 
 fn read_contents(cont: &str) -> (i32, i32) {
-    let part1 = get_part1(&cont);
-    let part2 = get_part2(&cont);;
+    let part1 = get_part1(cont);
+    let part2 = get_part2(cont);
     (part1, part2)
 }
 
@@ -38,46 +38,60 @@ fn get_part1(vec: &str) -> i32 {
     sum
 }
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
-enum Structure {
-    Array,
-    Dict,
+fn get_part2(vec: &str) -> i32 {
+    let re = Regex::new(r#"\{["\-\w\d:,]*\}"#).unwrap();
+    let re2 = Regex::new(r#"\[["\-\w\d,]*\]"#).unwrap();
+    let mut vec = vec.to_string();
+    loop {
+        if let Some(cap) = re.captures(&vec) {
+            let ca = cap.get(0).unwrap();
+            let res = analyze_dict(&cap[0]);
+            vec = vec.replace(ca.as_str(), &res.to_string());
+            continue;
+        }
+        if let Some(cap) = re2.captures(&vec) {
+            let ca = cap.get(0).unwrap();
+            let res = analyze_list(&cap[0]);
+            vec = vec.replace(ca.as_str(), &res.to_string());
+            continue;
+        }
+        println!("{vec}");
+        return vec.trim().parse::<i32>().unwrap();
+    }
 }
 
-fn get_part2(vec: &str) -> i32 {
-    let typ = if vec.chars().next().unwrap() == '[' {
-        Structure::Array
-    } else {
-        Structure::Dict
-    };
-    let mut sum = 0;
-    let mut i = 0;
-    let mut stack = 0;
-    let mut red = false;
-    let mut accu = String::new();
-    if typ == Structure::Array {
-        loop {
-            i += 1;
-            if i > vec.len() - 2 {
-                break;
-            }
-            let c = vec.chars().nth(i).unwrap();
-            dbg!(&c);
-            accu += &c.to_string();
-            if c == '{' || c == '[' {
-                stack += 1;
-            } else if c == '}' || c == ']' {
-                stack -= 1;
-            }
-            if stack < 0 {
-                panic!("Stack should never be negative");
-            }
+fn analyze_dict(str: &str) -> i32 {
+    let mut sum: i32 = 0;
+    assert!(!str.contains('[')); // All of these should have been removed
+    for a in str
+        .strip_prefix("{")
+        .unwrap()
+        .strip_suffix("}")
+        .unwrap()
+        .split(',')
+    {
+        let (_i, b) = a.split_once(':').unwrap();
+        if b == "\"red\"" {
+            return 0;
         }
+        sum += b.parse::<i32>().unwrap_or(0);
     }
+    sum
+}
 
-    dbg!(&typ);
-    0
-
+fn analyze_list(str: &str) -> i32 {
+    let mut sum: i32 = 0;
+    assert!(!str.contains('{')); // All of these should have been removed
+    for a in str
+        .strip_prefix("[")
+        .unwrap()
+        .strip_suffix("]")
+        .unwrap()
+        .split(',')
+    {
+        sum += a.parse::<i32>().unwrap_or(0);
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -96,10 +110,22 @@ mod tests {
     }
 
     #[test]
+    fn test_analyze_list() {
+        assert_eq!(analyze_list(&"[1,2,3]"), 6);
+        assert_eq!(analyze_list(&r#"[1,"red",5]"#), 6);
+    }
+
+    #[test]
+    fn test_analyze_dict() {
+        assert_eq!(analyze_dict(&r#"{"c":"red","b":2}"#), 0);
+        assert_eq!(analyze_dict(&r#"{"c":3,"b":2}"#), 5);
+    }
+
+    #[test]
     fn part2() {
-        assert_eq!(get_part2("[1,2,3]"), 6);
         assert_eq!(get_part2(r#"[1,{"c":"red","b":2},3]"#), 4);
+        assert_eq!(get_part2("[1,2,3]"), 6);
         assert_eq!(get_part2(r#"{"d":"red","e":[1,2,3,4],"f":5}"#), 0);
-        assert_eq!(get_part2(r#"[1,"red",5]"#), 0);
+        assert_eq!(get_part2(r#"[1,"red",5]"#), 6);
     }
 }
